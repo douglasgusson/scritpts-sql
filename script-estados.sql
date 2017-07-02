949782b06fad69ef842f60114ce0e5fdb937301e
Douglas Gusson <douglasgusson@gmail.com>
2017-07-02 13:08:04 -0300

    add script estados

---
+++ script-estados.sql
@@ -0,0 +1,28 @@
+-- INSERTS DE 27 ESTADOS --
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('AC', 'Acre');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('AL', 'Alagoas');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('AP', 'Amapá');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('AM', 'Amazonas');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('BA', 'Bahia');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('CE', 'Ceará');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('DF', 'Distrito Federal');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('ES', 'Espírito Santo');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('GO', 'Goiás');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('MA', 'Maranhão');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('MT', 'Mato Grosso');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('MS', 'Mato Grosso do Sul');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('MG', 'Minas Gerais');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('PA', 'Pará');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('PB', 'Paraíba');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('PR', 'Paraná');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('PE', 'Pernambuco');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('PI', 'Piauí');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('RJ', 'Rio de Janeiro');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('RN', 'Rio Grande do Norte');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('RS', 'Rio Grande do Sul');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('RO', 'Rondônia');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('RR', 'Roraima');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('SC', 'Santa Catarina');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('SP', 'São Paulo');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('SE', 'Sergipe');
+INSERT INTO uf (sigla_uf, nome_uf) VALUES ('TO', 'Tocantins');
